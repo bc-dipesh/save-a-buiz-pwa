1adbe81533e8c5878968bbb6e241c79a8b19dabd
bc-dipesh <bcdipeshwork@gmail.com>
2021-06-07 23:29:47 +0545

    added some strong tags around html elements

--- src/components/Fundraiser.js
+++ src/components/Fundraiser.js
@@ -88,7 +88,8 @@ const Fundraiser = ({ fundraiser, isCard }) => {
                   />
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  Donors:
+                  <strong>Donors</strong>
+                  :
                   {' '}
                   {numFormatter(donors)}
                 </ListGroup.Item>
@@ -122,7 +123,7 @@ const Fundraiser = ({ fundraiser, isCard }) => {
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                 </svg>
                 {' '}
-                {name}
+                <strong>{name}</strong>
                 {' '}
                 is organizing this
                 fundraiser.
@@ -132,7 +133,9 @@ const Fundraiser = ({ fundraiser, isCard }) => {
           <Row className="py-5">
             <Col md={8}>
               <p>
-                Description:
+                <strong>Description</strong>
+                :
+                {' '}
                 {description}
               </p>
             </Col>
