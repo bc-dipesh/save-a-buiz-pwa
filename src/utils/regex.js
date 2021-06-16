828ed76b55f214276a22b87ebb7050f3e365be21
bc-dipesh <bcdipeshwork@gmail.com>
2021-06-16 12:32:21 +0545

    added form validation for fundraiser
    added form validation for StartFundraiserScreen

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,4 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-const mobilePhoneNumberRegEx = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/g;
+const mobilePhoneNumberRegEx = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/;
+const urlRegEx =
+	/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
 
-export { mobilePhoneNumberRegEx };
+export { mobilePhoneNumberRegEx, urlRegEx };
