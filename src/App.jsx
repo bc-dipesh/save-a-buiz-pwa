ad7eb0f5f22f626edd016cd49593f21dbb22b42e
bc-dipesh <bcdipeshwork@gmail.com>
2021-06-16 20:54:37 +0545

    used React Suspense and lazy
    Used React Suspense and lazy to lazy load components and split codes for small bundle size

--- src/App.jsx
+++ src/App.jsx
@@ -1,172 +1,118 @@
 import { Button } from '@material-ui/core';
 import { useSnackbar, withSnackbar } from 'notistack';
-import React, { useState } from 'react';
+import React, { useState, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Footer from './components/Footer';
 import Header from './components/Header';
 import ScrollToTop from './components/ScrollToTop';
 import Notifier from './components/Notifier';
 import {
-	AboutScreen,
-	CommonQuestionScreen,
-	FundraiserListScreen,
-	FundraiserScreen,
-	HomeScreen,
-	HowItWorksScreen,
-	LegalContactInfoScreen,
-	PageNotFoundScreen,
-	StartFundraiserScreen,
-	SupportedProvinceScreen,
-	UserEditScreen,
-	UserFundraiserScreen,
-	UserListScreen,
-	UserProfileScreen,
-	UserRegisterScreen,
-	UserSignInScreen,
-	WhatIsCrowdfundingScreen,
+  FundraiserListScreen,
+  FundraiserScreen,
+  HomeScreen,
+  PageNotFoundScreen,
+  StartFundraiserScreen,
+  SupportedProvinceScreen,
+  UserSignInScreen,
+  UserRegisterScreen,
 } from './screens';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+const Footer = lazy(() => import('./components/Footer'));
+const AboutScreen = lazy(() => import('./screens/AboutScreen'));
+const UserProfileScreen = lazy(() => import('./screens/UserProfileScreen'));
+const HowItWorksScreen = lazy(() => import('./screens/HowItWorksScreen'));
+const UserListScreen = lazy(() => import('./screens/UserListScreen'));
+const UserEditScreen = lazy(() => import('./screens/UserEditScreen'));
+const CommonQuestionScreen = lazy(() => import('./screens/CommonQuestionScreen'));
+const LegalContactInfoScreen = lazy(() => import('./screens/LegalContactInfoScreen'));
+const WhatIsCrowdfundingScreen = lazy(() => import('./screens/WhatIsCrowdfundingScreen'));
+const UserFundraiserScreen = lazy(() => import('./screens/UserFundraiserScreen'));
+
 function App() {
-	const [isNewVersionAvailable, setIsNewVersionAvailable] = useState(false);
-	const [waitingWorker, setWaitingWorker] = useState({});
-	const { enqueueSnackbar } = useSnackbar();
+  const [isNewVersionAvailable, setIsNewVersionAvailable] = useState(false);
+  const [waitingWorker, setWaitingWorker] = useState({});
+  const { enqueueSnackbar } = useSnackbar();
 
-	const updateServiceWorker = () => {
-		if (waitingWorker) {
-			waitingWorker.postMessage({ type: 'SKIP_WAITING' });
-		}
-		setIsNewVersionAvailable(false);
-		window.location.reload();
-	};
+  const updateServiceWorker = () => {
+    if (waitingWorker) {
+      waitingWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+    setIsNewVersionAvailable(false);
+    window.location.reload();
+  };
 
-	// render the snackbar button
-	const refreshAction = () => (
-		<>
-			<Button
-				style={{ color: 'cyan' }}
-				className="snackbar-button"
-				size="small"
-				onClick={updateServiceWorker}
-			>
-				Refresh
-			</Button>
-		</>
-	);
+  // render the snackbar button
+  const refreshAction = () => (
+    <>
+      <Button
+        style={{ color: 'cyan' }}
+        className="snackbar-button"
+        size="small"
+        onClick={updateServiceWorker}
+      >
+        Refresh
+      </Button>
+    </>
+  );
 
-	const onServiceWorkerUpdate = (registration) => {
-		setWaitingWorker(registration && registration.waiting);
-		setIsNewVersionAvailable(true);
-		console.log(waitingWorker, isNewVersionAvailable);
-	};
+  const onServiceWorkerUpdate = (registration) => {
+    setWaitingWorker(registration && registration.waiting);
+    setIsNewVersionAvailable(true);
+    console.log(waitingWorker, isNewVersionAvailable);
+  };
 
-	// make app work offline
-	serviceWorkerRegistration.register({ onUpdate: onServiceWorkerUpdate });
+  // make app work offline
+  serviceWorkerRegistration.register({ onUpdate: onServiceWorkerUpdate });
 
-	// show snackbar with refresh button
-	if (isNewVersionAvailable) {
-		enqueueSnackbar(
-			'A new version of the app is available. Please refresh the page to see latest content',
-			{
-				persist: true,
-				variant: 'default',
-				action: refreshAction(),
-			}
-		);
-	}
+  // show snackbar with refresh button
+  if (isNewVersionAvailable) {
+    enqueueSnackbar(
+      'A new version of the app is available. Please refresh the page to see latest content',
+      {
+        persist: true,
+        variant: 'default',
+        action: refreshAction(),
+      }
+    );
+  }
 
-	return (
-		<>
-			<Router>
-				<ScrollToTop />
-				<Header />
-				<main>
-					<Notifier />
-					<Switch>
-						<Route path="/" component={HomeScreen} exact />
-						<Route
-							path="/search/:keyword"
-							component={FundraiserListScreen}
-							exact
-						/>
-						<Route
-							path="/fundraisers"
-							component={FundraiserListScreen}
-							exact
-						/>
-						<Route
-							path="/fundraisers/:id"
-							component={FundraiserScreen}
-							exact
-						/>
-						<Route
-							path="/start-fundraiser"
-							component={StartFundraiserScreen}
-							exact
-						/>
-						<Route
-							path="/sign-in"
-							component={UserSignInScreen}
-							exact
-						/>
-						<Route
-							path="/user/profile"
-							component={UserProfileScreen}
-							exact
-						/>
-						<Route
-							path="/user/fundraisers"
-							component={UserFundraiserScreen}
-							exact
-						/>
-						<Route
-							path="/register"
-							component={UserRegisterScreen}
-							exact
-						/>
-						<Route path="/about" component={AboutScreen} exact />
-						<Route
-							path="/how-it-works"
-							component={HowItWorksScreen}
-							exact
-						/>
-						<Route
-							path="/what-is-crowdfunding"
-							component={WhatIsCrowdfundingScreen}
-							exact
-						/>
-						<Route
-							path="/legal"
-							component={LegalContactInfoScreen}
-							exact
-						/>
-						<Route
-							path="/common-questions"
-							component={CommonQuestionScreen}
-							exact
-						/>
-						<Route
-							path="/supported-provinces"
-							component={SupportedProvinceScreen}
-							exact
-						/>
-						<Route
-							path="/admin/user-list"
-							component={UserListScreen}
-							exact
-						/>
-						<Route
-							path="/admin/user/:id/edit"
-							component={UserEditScreen}
-							exact
-						/>
-						<Route component={PageNotFoundScreen} />
-					</Switch>
-				</main>
-				<Footer />
-			</Router>
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <Header />
+        <main>
+          <Notifier />
+          <Switch>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/search/:keyword" component={FundraiserListScreen} exact />
+            <Route path="/fundraisers" component={FundraiserListScreen} exact />
+            <Route path="/fundraisers/:id" component={FundraiserScreen} exact />
+            <Route path="/start-fundraiser" component={StartFundraiserScreen} exact />
+            <Route path="/sign-in" component={UserSignInScreen} exact />
+            <Route path="/supported-provinces" component={SupportedProvinceScreen} exact />
+            <Route path="/register" component={UserRegisterScreen} exact />
+
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <Route path="/about" component={AboutScreen} exact />
+              <Route path="/user/profile" component={UserProfileScreen} exact />
+              <Route path="/user/fundraisers" component={UserFundraiserScreen} exact />
+              <Route path="/how-it-works" component={HowItWorksScreen} exact />
+              <Route path="/what-is-crowdfunding" component={WhatIsCrowdfundingScreen} exact />
+              <Route path="/legal" component={LegalContactInfoScreen} exact />
+              <Route path="/common-questions" component={CommonQuestionScreen} exact />
+              <Route path="/admin/user-list" component={UserListScreen} exact />
+              <Route path="/admin/user/:id/edit" component={UserEditScreen} exact />
+            </Suspense>
+            <Route component={PageNotFoundScreen} />
+          </Switch>
+        </main>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Footer />
+        </Suspense>
+      </Router>
+    </>
+  );
 }
 
 export default withSnackbar(App);
--- src/screens/LegalContactInfoScreen.jsx
+++ src/screens/LegalContactInfoScreen.jsx
@@ -2,50 +2,45 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
 const LegalContactInfoScreen = () => (
-	<>
-		<Container style={{ backgroundColor: '#fbf8f6' }} fluid>
-			<Container className="py-5">
-				<Row>
-					<h1>Legal Contact Info</h1>
-					<p>
-						Contact information for law enforcement, legal counsel,
-						compliance and regulatory bodies.
-					</p>
-				</Row>
-			</Container>
-		</Container>
-		<Container className="mt-5">
-			<Row>
-				<h4>Legal Contact Directory</h4>
-				<p>
-					We take your legal questions and concerns very seriously at
-					SaveABuiz. For a directory of who to contact for your
-					specific inquiry, please review the secions below.
-				</p>
-			</Row>
-			<Row className="my-3" />
-			<Row>
-				<h4>Site Issues</h4>
-				<p>
-					If you have a problem concerning an account, how SaveABuiz
-					works or any other general issue, please visit our Help
-					Center.
-				</p>
-				<p>
-					If you are looking to get a campaign removed for fraudulent
-					or untrustworthy behavior, report the campaign here.
-				</p>
-			</Row>
-			<Row className="my-3" />
-			<Row>
-				<h4>Other Law-Related Questions</h4>
-				<p>
-					If you have other law-related questions, please send an
-					email to legal@saveabuiz.com.
-				</p>
-			</Row>
-		</Container>
-	</>
+  <>
+    <Container className="mt-5" style={{ backgroundColor: '#fbf8f6' }} fluid>
+      <Container className="py-5">
+        <Row>
+          <h1>Legal Contact Info</h1>
+          <p>
+            Contact information for law enforcement, legal counsel, compliance and regulatory
+            bodies.
+          </p>
+        </Row>
+      </Container>
+    </Container>
+    <Container className="mt-5">
+      <Row>
+        <h4>Legal Contact Directory</h4>
+        <p>
+          We take your legal questions and concerns very seriously at SaveABuiz. For a directory of
+          who to contact for your specific inquiry, please review the secions below.
+        </p>
+      </Row>
+      <Row className="my-3" />
+      <Row>
+        <h4>Site Issues</h4>
+        <p>
+          If you have a problem concerning an account, how SaveABuiz works or any other general
+          issue, please visit our Help Center.
+        </p>
+        <p>
+          If you are looking to get a campaign removed for fraudulent or untrustworthy behavior,
+          report the campaign here.
+        </p>
+      </Row>
+      <Row className="my-3" />
+      <Row>
+        <h4>Other Law-Related Questions</h4>
+        <p>If you have other law-related questions, please send an email to legal@saveabuiz.com.</p>
+      </Row>
+    </Container>
+  </>
 );
 
 export default LegalContactInfoScreen;
