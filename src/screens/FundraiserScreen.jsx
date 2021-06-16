76329618d591a3b8121668a39a12aa19c9954e56
bc-dipesh <bcdipeshwork@gmail.com>
2021-06-16 12:46:01 +0545

    added new prop to Fundraiser
    added youtube video link property to Fundraiser component

--- src/screens/FundraiserScreen.jsx
+++ src/screens/FundraiserScreen.jsx
@@ -10,203 +10,171 @@ import Fundraiser from '../components/Fundraiser';
 import Message from '../components/Message';
 
 const Children = ({ loading, error, fundraiser }) => {
-	if (loading) {
-		return (
-			<>
-				<Container>
-					<Row>
-						<Col md={8}>
-							<Skeleton variant="rect" height={200} />
-						</Col>
-						<Col md={4}>
-							<Card>
-								<ListGroup variant="flush">
-									<ListGroup.Item>
-										<Row>
-											<Col>
-												<Skeleton
-													variant="text"
-													width="40%"
-												/>
-												<Skeleton
-													variant="text"
-													width="50%"
-													height={10}
-												/>
-											</Col>
-										</Row>
-									</ListGroup.Item>
-									<ListGroup.Item>
-										<Skeleton variant="text" />
-									</ListGroup.Item>
-									<ListGroup.Item>
-										<Skeleton variant="text" width="40%" />
-									</ListGroup.Item>
-									<ListGroup.Item>
-										<Row>
-											<Col>
-												<Skeleton
-													variant="rect"
-													height={48}
-												/>
-											</Col>
-										</Row>
-									</ListGroup.Item>
-								</ListGroup>
-							</Card>
-						</Col>
-					</Row>
-				</Container>
-				<Container
-					className="my-5"
-					style={{ backgroundColor: '#fbf8f6' }}
-					fluid
-				>
-					<Container>
-						<Row className="py-5">
-							<Col md={8}>
-								<Skeleton variant="text" width="30%" />
-							</Col>
-						</Row>
-						<Row>
-							<Col md={8}>
-								<Skeleton variant="text" width="30%" />
-							</Col>
-						</Row>
-						<Row className="py-5">
-							<Col md={8}>
-								<Skeleton variant="text" />
-								<Skeleton variant="text" />
-								<Skeleton variant="text" />
-								<Skeleton variant="text" />
-								<Skeleton variant="text" />
-							</Col>
-						</Row>
-					</Container>
-				</Container>
-				<Container>
-					<Row>
-						<Col md={8}>
-							<ListGroup variant="flush">
-								<ListGroup.Item>
-									<Skeleton variant="text" width="20%" />
-								</ListGroup.Item>
-								<ListGroup.Item>
-									<Row>
-										<Col>
-											<Skeleton
-												variant="text"
-												width="20%"
-											/>
-										</Col>
-										<Col>
-											<Skeleton
-												variant="rect"
-												width="50%"
-												height={48}
-											/>
-										</Col>
-									</Row>
-								</ListGroup.Item>
-							</ListGroup>
-						</Col>
-					</Row>
-				</Container>
-				<Container>
-					<Row>
-						<Col md={8}>
-							<ListGroup variant="flush">
-								<ListGroup.Item>
-									<Skeleton variant="text" width="20%" />
-								</ListGroup.Item>
-								{[1, 2, 3].map(() => (
-									<ListGroup.Item
-										className="py-3"
-										key={uuidv4()}
-									>
-										<Row>
-											<Skeleton
-												variant="text"
-												width="20%"
-											/>
-										</Row>
-										<Row>
-											<Skeleton
-												variant="text"
-												width="50%"
-											/>
-										</Row>
-									</ListGroup.Item>
-								))}
-							</ListGroup>
-						</Col>
-					</Row>
-				</Container>
-			</>
-		);
-	}
-	if (!error) {
-		return <Fundraiser fundraiser={fundraiser} isCard={false} />;
-	}
-	return (
-		<Container className="my-5">
-			<Message variant="danger">Something went wrong</Message>
-			<Button
-				variant="outline-primary"
-				onClick={() => window.location.reload()}
-			>
-				Refresh page ?
-			</Button>
-		</Container>
-	);
+  if (loading) {
+    return (
+      <>
+        <Container>
+          <Row>
+            <Col md={8}>
+              <Skeleton variant="rect" height={200} />
+            </Col>
+            <Col md={4}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>
+                        <Skeleton variant="text" width="40%" />
+                        <Skeleton variant="text" width="50%" height={10} />
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Skeleton variant="text" />
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Skeleton variant="text" width="40%" />
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>
+                        <Skeleton variant="rect" height={48} />
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        <Container className="my-5" style={{ backgroundColor: '#fbf8f6' }} fluid>
+          <Container>
+            <Row className="py-5">
+              <Col md={8}>
+                <Skeleton variant="text" width="30%" />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={8}>
+                <Skeleton variant="text" width="30%" />
+              </Col>
+            </Row>
+            <Row className="py-5">
+              <Col md={8}>
+                <Skeleton variant="text" />
+                <Skeleton variant="text" />
+                <Skeleton variant="text" />
+                <Skeleton variant="text" />
+                <Skeleton variant="text" />
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+        <Container>
+          <Row>
+            <Col md={8}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Skeleton variant="text" width="20%" />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>
+                      <Skeleton variant="text" width="20%" />
+                    </Col>
+                    <Col>
+                      <Skeleton variant="rect" width="50%" height={48} />
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <Col md={8}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Skeleton variant="text" width="20%" />
+                </ListGroup.Item>
+                {[1, 2, 3].map(() => (
+                  <ListGroup.Item className="py-3" key={uuidv4()}>
+                    <Row>
+                      <Skeleton variant="text" width="20%" />
+                    </Row>
+                    <Row>
+                      <Skeleton variant="text" width="50%" />
+                    </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+  if (!error) {
+    return <Fundraiser fundraiser={fundraiser} isCard={false} />;
+  }
+  return (
+    <Container className="my-5">
+      <Message variant="danger">Something went wrong</Message>
+      <Button variant="outline-primary" onClick={() => window.location.reload()}>
+        Refresh page ?
+      </Button>
+    </Container>
+  );
 };
 
 const FundraiserScreen = ({ match }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const fundraiserDetails = useSelector((state) => state.fundraiserDetails);
-	const { loading, error, fundraiser } = fundraiserDetails;
+  const fundraiserDetails = useSelector((state) => state.fundraiserDetails);
+  const { loading, error, fundraiser } = fundraiserDetails;
 
-	useEffect(() => {
-		dispatch(listFundraiserDetails(match.params.id));
-	}, [dispatch, match]);
+  useEffect(() => {
+    dispatch(listFundraiserDetails(match.params.id));
+  }, [dispatch, match]);
 
-	return (
-		<>
-			<Container>
-				<Link className="btn btn-outline-primary my-3" to="/">
-					Go Back
-				</Link>
-			</Container>
-			<Children loading={loading} error={error} fundraiser={fundraiser} />
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <Link className="btn btn-outline-primary my-3" to="/">
+          Go Back
+        </Link>
+      </Container>
+      <Children loading={loading} error={error} fundraiser={fundraiser} />
+    </>
+  );
 };
 
 Children.propTypes = {
-	loading: PropTypes.bool,
-	error: PropTypes.string,
-	fundraiser: PropTypes.shape({
-		_id: PropTypes.string,
-		image: PropTypes.string,
-		title: PropTypes.string,
-		shortDescription: PropTypes.string,
-		description: PropTypes.string,
-		collected: PropTypes.number,
-		goal: PropTypes.number,
-		donors: PropTypes.number,
-		organizer: PropTypes.shape({
-			name: PropTypes.string,
-		}),
-	}).isRequired,
+  loading: PropTypes.bool,
+  error: PropTypes.string,
+  fundraiser: PropTypes.shape({
+    _id: PropTypes.string,
+    image: PropTypes.string,
+    title: PropTypes.string,
+    shortDescription: PropTypes.string,
+    description: PropTypes.string,
+    collected: PropTypes.number,
+    goal: PropTypes.number,
+    donors: PropTypes.number,
+    youTubeVideoLink: PropTypes.string,
+    organizer: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+  }).isRequired,
 };
 
 FundraiserScreen.propTypes = {
-	match: PropTypes.shape({
-		params: PropTypes.shape({
-			id: PropTypes.string.isRequired,
-		}),
-	}),
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }),
 };
 
 export default FundraiserScreen;
