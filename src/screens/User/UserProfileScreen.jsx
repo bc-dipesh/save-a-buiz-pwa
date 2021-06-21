32ebea30ba477295e8ff4b5012438d6d7d63b952
bc-dipesh <bcdipeshwork@gmail.com>
2021-06-21 13:31:03 +0545

    Fix user email confirmation bug
    Add optional chaining operator to
    access an empty user object to fix
    cannot access property of undefined bug

--- src/screens/User/UserProfileScreen.jsx
+++ src/screens/User/UserProfileScreen.jsx
@@ -179,19 +179,9 @@ const UserProfileScreen = ({ history }) => {
               <Form.Label>Email Address</Form.Label>
               {loading ? (
                 <Skeleton variant="text" />
-              ) : !userInfo.user.isEmailConfirmed ? (
+              ) : !userInfo?.user?.isEmailConfirmed ? (
                 <Alert variant="info">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-exclamation"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.553.553 0 0 1-1.1 0L7.1 4.995z" />
-                  </svg>{' '}
-                  We&apos;ve sent you a email verification link to the email address you provided.
+                  !We&apos;ve sent you a email verification link to the email address you provided.
                   Please verify your email then logout and sign in again after email verification.
                 </Alert>
               ) : (
