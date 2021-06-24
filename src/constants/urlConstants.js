963b277b7dddf57ceb6f8f03ad3c772628a12c91
bc-dipesh <bcdipeshwork@gmail.com>
2021-06-24 16:44:38 +0545

    Add push notification service

--- src/constants/urlConstants.js
+++ src/constants/urlConstants.js
@@ -4,4 +4,9 @@ const USERS_ROUTE = 'https://save-a-buiz-api.herokuapp.com/api/v1/users';
 const FUNDRAISERS_ROUTE = 'https://save-a-buiz-api.herokuapp.com/api/v1/fundraisers';
 const ANALYTICS_ROUTE = 'https://save-a-buiz-api.herokuapp.com/api/v1/analytics';
 
+// const AUTH_ROUTE = 'http://localhost:5000/api/v1/auth';
+// const USERS_ROUTE = 'http://localhost:5000/api/v1/users';
+// const FUNDRAISERS_ROUTE = 'http://localhost:5000/api/v1/fundraisers';
+// const ANALYTICS_ROUTE = 'http://localhost:5000/api/v1/analytics';
+
 export { AUTH_ROUTE, USERS_ROUTE, FUNDRAISERS_ROUTE, ANALYTICS_ROUTE };
